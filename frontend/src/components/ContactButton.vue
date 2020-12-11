f7310e0eb3230673d1bd9319de2694e5b6639594
Leonardo <leocammpelo@hotmail.com>
2020-12-10 22:07:51 -0300

    Alterações nos pacotes, adição do componente Navbar e ContactButton.

---
+++ frontend/src/components/ContactButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <el-button class="contato-button" @click="navigateTo()">CONTATE-NOS</el-button>
+</template>
+
+<style lang="scss">
+    .contato-button {
+        font-family: Roboto;
+        font-size: 0.9em !important;
+        font-weight: 700 !important;
+        width: 200px;
+        padding-top: 8px !important;
+        padding-bottom: 8px !important;
+        border-width: 2px !important;
+        color: #fff !important;
+        border-color:#f0f0f0 !important; 
+        background: transparent !important;
+    }
+
+    .contato-button:hover {
+        background: linear-gradient(270deg, #C68953 0%, #F3AF73 31.77%, #D8965C 100%) !important;
+    }
+
+</style>
+
+<script>
+export default {
+    name: 'ContactButton',
+
+    methods: {
+        navigateTo() {
+             window.location.href = "http://localhost:8080/#/";
+        }
+    },
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,24 @@
 <template>
-    <Navbar> </Navbar>
+  <el-row>
+    <Navbar style="z-index: 99"></Navbar>
+    <Carousel />
+  </el-row>
+  
 </template>
+
+<style lang="scss">
+@import '../styles/variables.scss';
+</style>
 
 <script>
 import Navbar from "../components/Navbar"
+import Carousel from "../components/Home/Carousel"
 
 export default {
   name: 'Home',
   components: {
     Navbar,
+    Carousel,
   }
 }
 </script>
