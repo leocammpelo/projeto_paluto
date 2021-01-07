9f5097cebfc2e704d090f9d2433bf1b3af5f7ef0
Leonardo <leocammpelo@hotmail.com>
2021-01-07 14:00:34 -0300

    Criação Footer.

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-container>
+    <el-row class="footer">
+      <div class="rodape">
+        <el-row>
+          <div class="aviso_footer">
+            Paluto Móveis®
+          </div>
+        </el-row>
+        <el-row>
+          <div class="mensagem_footer">Desing e desenvolvimento por</div>
+          <a href="https://api.whatsapp.com/send?phone=5535988248993&text=Ol%C3%A1!%20Como%20posso%20ajudar%3F%20" target="_blank">
+            DIGPEL - Soluções Digitais
+          </a>
+        </el-row>
+      </div>
+    </el-row>
+  </el-container>
+</template>
+
+<style lang="scss">
+    @import '../styles/variables.scss';
+
+.icone-comp {
+  height: 30px;
+}
+
+.footer {
+  background-color: #D8965C;
+  height: 100%;
+  width: 100%;
+  padding-right: 20px;
+  display: flex;
+  justify-content: flex-end;
+}
+.aviso_footer {
+  font-weight: lighter;
+  margin-top: 5px;
+  color: #f0f0f0;
+  font-size: 14px;
+}
+
+.rodape {
+  display: flex;
+  justify-content: space-between;
+  width: 98%;
+  align-items: center;
+  text-align: right;
+}
+
+.mensagem_footer {
+  font-weight: lighter;
+  margin-top: 5px;
+  color: #f0f0f0;
+  font-size: 11.5px;
+}
+@media screen and (max-width: 500px) {
+  .icone-comp {
+    height: 20px;
+  }
+
+  .footer {
+    padding-right: 0px;
+  }
+
+  .aviso_footer {
+    font-weight: lighter;
+    margin-top: 5px;
+    color: #f0f0f0;
+    font-size: 10px;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "Footer",
+
+  data() {
+    return {
+        
+    };
+  }
+};
+</script>
