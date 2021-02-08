12a184da3564945af4fbb19bc7d5b926b15ada7b
Leonardo <leocammpelo@hotmail.com>
2021-02-08 13:07:17 -0300

    Facebook Pixel e Google Analytics implementados.

---
+++ frontend/src/components/Cookies.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+    data() {
+        return {
+            status: null,
+        }
+    },
+
+    methods: {
+        cookieStatus (status) {
+        console.log('status: ' + status)
+        this.status = status
+        },
+        cookieClickedAccept () {
+        console.log('here in accept')
+        this.status = 'accept'
+        },
+        cookieClickedDecline () {
+        console.log('here in decline')
+        this.status = 'decline'
+        },
+    },
+
+    computed: {
+        statusText () {
+            return this.status || 'No cookie set'
+        }
+    }
+}
+    
+</script>
